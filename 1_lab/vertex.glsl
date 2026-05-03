1dc45168db75a0984b4f82e4d15adb20c6c5ee5b
penilopa322 <bemsdems2006@gmail.com>
2026-05-03 20:47:27 +0800

    Update vertex.glsl

--- 1_lab/vertex.glsl
+++ 1_lab/vertex.glsl
@@ -1,5 +1,10 @@
 #version 460 core
-layout (location = 0) in vec3 vp;
-void main() {
-    gl_Position = vec4(vp, 1.0);
+layout (location = 0) in vec3 aPos;
+uniform mat4 projection;
+uniform mat4 view;
+uniform mat4 model;
+uniform mat4 transform;
+void main()
+{
+    gl_Position = projection * view * model * transform * vec4(aPos, 1.0);
 }
